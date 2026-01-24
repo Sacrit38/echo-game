41e7307a992f77ca7b31cf43aaa6a64cdedf97f7
ghazi23092005 <muhammadddghazi87@gmail.com>
2026-01-24 00:20:04 -0800

    nocamerafollow

--- Assets/Scripts/BasicPlayerMovement.cs
+++ Assets/Scripts/BasicPlayerMovement.cs
@@ -2,20 +2,51 @@ using UnityEngine;
 
 public class BasicPlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
     public float moveSpeed = 5f;
-    public Rigidbody2D rb;
-    Vector2 movement;
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+
+    [Header("Echolocation Settings")]
+    public float stepInterval = 0.5f; // Jeda antar langkah kaki
+    private float stepTimer;
+    
+    // public EcholocationSystem echoSystem; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        // Input dari keyboard/WASD
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        if (moveInput.sqrMagnitude > 0)
+        {
+            stepTimer -= Time.deltaTime;
+            if (stepTimer <= 0)
+            {
+                TriggerFootstepEcho();
+                stepTimer = stepInterval;
+            }
+        }
+        else
+        {
+            stepTimer = 0;
+        }
     }
 
     void FixedUpdate()
     {
-        // Menggerakkan karakter tanpa gravitasi
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + moveInput.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void TriggerFootstepEcho()
+    {
+        Debug.Log("Langkah kaki terdeteksi: Memicu Gelombang Ekolokasi!");
+        // memanggil fungsi untuk memunculkan visual gelombang (Putih/Normal)
+        // echoSystem.CreateRipple(transform.position, Color.white);
     }
 }
