5ae2550c246bd2ddb77fc7babf10e4025cb2c110
daffa.doj <daffa.doj@gmail.com>
2026-01-24 20:27:09 +0700

    feat: wip main menu + added text mesh pro libraries

---
+++ Assets/Scripts/MenuInputManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInputManager : MonoBehaviour
+{
+    public GameObject optionPanel;
+    public void Play()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    
+    public void ToggleOptions()
+    {
+        optionPanel.SetActive(!optionPanel.activeSelf);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
