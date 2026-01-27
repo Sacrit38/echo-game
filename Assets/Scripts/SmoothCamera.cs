867557ab35d1727e7ae19a7aae88237a739e4f69
daffa.doj <daffa.doj@gmail.com>
2026-01-27 13:04:41 +0700

    feat: basic odeles room setup + bg and obstacle asset

--- Assets/Scripts/SmoothCamera.cs
+++ Assets/Scripts/SmoothCamera.cs
@@ -10,12 +10,21 @@ public class SmoothCamera : MonoBehaviour
     public float smoothSpeed = 0.05f; 
     public Vector3 offset = new Vector3(0, 0, -10);
 
+    [Header("Clamp Settings")]
+    public bool clamped = false;
+    public Vector2 start, end; 
+
     private void LateUpdate()
     {
         if (target == null) return;
 
     
         Vector3 desiredPosition = target.position + offset;
+
+        if (clamped)
+        {
+            desiredPosition = new Vector3(Mathf.Clamp(desiredPosition.x, start.x, end.x), Mathf.Clamp(desiredPosition.y, start.y, end.y), desiredPosition.z);
+        }
 
         
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
